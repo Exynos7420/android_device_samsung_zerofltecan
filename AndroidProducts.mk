fa10cf1dec17d70daa73b8d46c27495d9fdebf13
enesuzun2002 <enesuzun200227@gmail.com>
2020-02-20 11:59:39 +0000

    zerofltecan: switch to COMMON_LUNCH_CHOICES for Q
    Change-Id: Iae0d4e950cc7639b8f0842430978e889738957bf

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,6 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/full_zerofltecan.mk \
-    $(LOCAL_DIR)/lineage_zerofltecan.mk \
-    $(LOCAL_DIR)/viper_zerofltecan.mk \
-    $(LOCAL_DIR)/aosp_zerofltecan.mk
+    $(LOCAL_DIR)/lineage_zerofltecan.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_zerofltecan-userdebug
